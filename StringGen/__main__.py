3f4458b93400cf020900ecbe52ca5a3582dacf92
Snowball <riteshraushan60@gmail.com>
2025-04-02 05:11:05 +0530

    first commit

--- StringGen/__main__.py
+++ StringGen/__main__.py
@@ -22,5 +22,5 @@ async def anony_boot():
 
 
 if __name__ == "__main__":
-    asyncio.run(anony_boot())  # Correct way in Python 3.10+
+    asyncio.get_event_loop().run_until_complete(anony_boot())
     LOGGER.info("Stopping String Gen Bot...")
